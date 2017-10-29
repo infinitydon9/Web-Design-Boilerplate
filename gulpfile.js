bc184b5c3d397c7578ecd1739bf940df50ac06f5
Ashish Ranjan <infinitydon9@gmail.com>
2017-10-29 11:01:21 +0530

    Added task for minification of js

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
 var sass = require('gulp-sass');
+var minify = require('gulp-minify');
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
@@ -12,9 +13,22 @@ gulp.task('sass', function () {
 
 // Move the javascript files into our /src/js folder
 gulp.task('js', function () {
-    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js','node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
         .pipe(gulp.dest("./js"))
         .pipe(browserSync.stream());
+});
+
+// Compress the js file and convert them in minified ones
+gulp.task('compress', function() {
+    gulp.src('./amd/*.js')
+        .pipe(minify({
+            ext:{
+                min:'.min.js'
+            },
+            exclude: ['tasks'],
+            ignoreFiles: ['.combo.js', '-min.js']
+        }))
+    .pipe(gulp.dest('./js'))
 });
 
 // Static Server + watching scss/html files
@@ -25,6 +39,7 @@ gulp.task('serve', ['sass'], function () {
     });
 
     gulp.watch(['node_modules/bootstrap/scss/bootstrap.scss', './scss/*.scss'], ['sass']);
+    gulp.watch(['./amd/*.js'], ['compress']);
     gulp.watch("./*.html").on('change', browserSync.reload);
 });
 
